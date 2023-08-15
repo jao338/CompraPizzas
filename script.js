b1dc786192b9adc476a5b1eb38282f2393a24eb9
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-15 12:00:20 -0300

    3 - Finalizando o modal

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const c = (el) => document.querySelector(el);
 const ca = (el) => document.querySelectorAll(el);
 
+let modalQt = 1;
+
 
 pizzaJson.map((item, index) => {
 
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.setAttribute('data-key', index)
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
@@ -14,11 +17,37 @@ pizzaJson.map((item, index) => {
         
         e.preventDefault();
 
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
+
+
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        ca('.pizzaInfo--size').forEach((size, sizeIndex) => {
+
+            if(sizeIndex == 2){
+                size.classList.add('selected');
+            }
+
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
+
+
+        c('.pizzaInfo--qt').innerHTML.modalQt;
+
+
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';
-        setTimeout(()=>{
+
+        setTimeout(() => {
+
             c('.pizzaWindowArea').style.opacity = 1;
+
         }, 200);
+
     });
 
     c('.pizza-area').append(pizzaItem);
